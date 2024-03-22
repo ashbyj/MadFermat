8c1d9be197bf26e1ec75500971171399b48d1a95
Jesse Ashby <ashbyj@gmail.com>
2024-03-22 19:27:57 +0000

    made the matrix size function dynamic and moved closer to the rref answer

--- PythonAnswers.py
+++ PythonAnswers.py
@@ -1,48 +1,42 @@
 import numpy as np
+np.random.seed(42)
 
 # this function took 20 minutes 
-def build_and_sum_maxtrix():
-    matrix_200 = np.random.randint(10, size=(200,200))
-    matrix_total = 0
+def build_and_sum_maxtrix(rows, columns):
+    matrix_f = np.random.randint(10, size=(rows, columns))
+    matrix_sum = 0
     
-    for row in range(200):
-        for column in range(200):
-            matrix_total += matrix_200[row][column]
+    for row in range(rows):
+        for column in range(columns):
+            matrix_sum += matrix_f[row][column]
 
-    print (f'matrix size: {len(matrix_200)}, {len(matrix_200[0])}')
-    print (f'matrix total: {matrix_total}')
+    print (f'matrix size: {len(matrix_f)}, {len(matrix_f[0])}')
+    print (f'matrix sum: {matrix_sum}')
+    print (matrix_f)
     
-    return matrix_200
+    return matrix_f
 
-#this function does not work yet.  i have 3 hours into it so far
 def row_redux(matrix):
-    x = 0
-    
-    for y in range(200):
-        if x == 200:
+
+    return_matrix = matrix.copy().astype(float)
+    row_count = len(return_matrix)
+
+   # run rref algo on every row
+    for pivot_row in range(row_count):
+
+        # divide the current row by its pivot value
+        pivot_value = return_matrix[pivot_row][pivot_row]
+        return_matrix[pivot_row] /= pivot_value
+
+        # end the loop before trying to extend out of bounds
+        if pivot_row == row_count:
             break
-            
-        pivot_row = x  
-        while pivot_row < 200 and matrix[pivot_row, y] == 0:
-            pivot_row += 1
-        if pivot_row == 200:
-            continue
-    
-        matrix[[x, pivot_row]] = matrix[[pivot_row, x]]
-        matrix[x] /= matrix[x, y]
 
-        for i in range(200):
-            if i != x and matrix[i, y] != 0:
-                matrix[i] -= matrix[i, y] * matrix[x]
-
-        x += 1
-    return matrix
-
+        for row in range(pivot_row + 1, row_count):            
+            return_matrix[row] -= return_matrix[pivot_row] * pivot_value 
+  
+    return return_matrix
 
 if __name__ == "__main__":
-    matrix = build_and_sum_maxtrix()
-    #rref_matrix = row_redux(matrix.copy())
-    print ('random 200x200:')
-    print(matrix)
-    #print ('row reduced echelon form:')
-    #print(rref_maxtrix)
+    matrix = build_and_sum_maxtrix(200, 200)
+    print(row_redux(matrix))
